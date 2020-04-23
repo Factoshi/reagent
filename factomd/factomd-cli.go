b3ee7a16c30e41fbabcd253e7571340a4a66ecc9
Paul Bernier <bernier.pja@gmail.com>
2020-04-23 10:35:16 -0700

    feat: add FACTOMD_RPC_ENDPOINT env variable

--- factomd/factomd-cli.go
+++ factomd/factomd-cli.go
@@ -2,6 +2,8 @@ package factomd
 
 import (
 	"encoding/hex"
+	"net/url"
+	"os"
 
 	_log "github.com/PaulBernier/chockagent/log"
 
@@ -9,11 +11,21 @@ import (
 )
 
 var (
-	c   jsonrpc2.Client
-	log = _log.GetLog()
+	c           jsonrpc2.Client
+	log         = _log.GetLog()
+	rpcEndpoint = "http://localhost:8088/v2"
 )
 
-const ENDPOINT = "http://localhost:8088/v2"
+func init() {
+	if os.Getenv("FACTOMD_RPC_ENDPOINT") != "" {
+		_, err := url.ParseRequestURI(os.Getenv("FACTOMD_RPC_ENDPOINT"))
+		if err != nil {
+			log.WithError(err).Fatalf("Failed to parse FACTOMD_RPC_ENDPOINT: [%s]",
+				os.Getenv("FACTOMD_RPC_ENDPOINT"))
+		}
+		rpcEndpoint = os.Getenv("FACTOMD_RPC_ENDPOINT")
+	}
+}
 
 type DBlockByHeightResult struct {
 	DBlock struct {
@@ -30,7 +42,7 @@ type CurrentMinuteResult struct {
 
 func IsMainnet() (bool, error) {
 	var result DBlockByHeightResult
-	err := c.Request(nil, ENDPOINT, "dblock-by-height", struct {
+	err := c.Request(nil, rpcEndpoint, "dblock-by-height", struct {
 		Height int `json:"height"`
 	}{Height: 0}, &result)
 
@@ -43,7 +55,7 @@ func IsMainnet() (bool, error) {
 
 func CurrentBlockAndMinute() (int, int, error) {
 	var result CurrentMinuteResult
-	err := c.Request(nil, ENDPOINT, "current-minute", nil, &result)
+	err := c.Request(nil, rpcEndpoint, "current-minute", nil, &result)
 
 	if err != nil {
 		return 0, 0, err
@@ -54,7 +66,7 @@ func CurrentBlockAndMinute() (int, int, error) {
 
 func CommitAndRevealEntry(commit []byte, reveal []byte) error {
 	// Commit
-	err := c.Request(nil, ENDPOINT, "commit-entry", struct {
+	err := c.Request(nil, rpcEndpoint, "commit-entry", struct {
 		Message string `json:"message"`
 	}{Message: hex.EncodeToString(commit)}, nil)
 
@@ -63,7 +75,7 @@ func CommitAndRevealEntry(commit []byte, reveal []byte) error {
 	}
 
 	// Reveal
-	return c.Request(nil, ENDPOINT, "reveal-entry", struct {
+	return c.Request(nil, rpcEndpoint, "reveal-entry", struct {
 		Entry string `json:"entry"`
 	}{Entry: hex.EncodeToString(reveal)}, nil)
 }
